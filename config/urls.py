c58405014f59b42d9b8fdc3289c87e367e97d098
Johan Suarez <jhoanseb1992@gmail.com>
2023-10-04 20:16:34 +1100

    update user app

--- config/urls.py
+++ config/urls.py
@@ -8,6 +8,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("users.urls")),
+    path("users/", include("users.urls", namespace="users")),
     path("",TemplateView.as_view(template_name="home.html"),name="home"),
+    path("account/", include("allauth.urls"))
 ]
