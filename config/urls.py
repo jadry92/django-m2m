07b7986edb93bcd81b46805e2215f54604de393b
johan suarez <jhoanseb1992@gmail.com>
2024-01-21 15:49:02 +1100

    add media urls

--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,12 @@ config URL Configuration
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
-    path("",TemplateView.as_view(template_name="home.html"),name="home"),
-    path("account/", include("allauth.urls"))
-]
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("account/", include("allauth.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
